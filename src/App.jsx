7c34c1f50a38e333438ba85a7e3167cc3a41d805
kittiphopp <parkpor26@gmail.com>
2024-02-28 09:32:01 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import Contact from './assets/Contact';
 import Hello from './assets/Hello';
 
+
 function App() {
+  const helloData = [
+    { name: 'Kittiphop', message: 'Hi there' },
+    { name: 'Tom', message: 'Hello..' }
+  ];
+
   return (
-    <div className='App'>
-      <Hello / >
+    <div>
+      {helloData.map((data, index) => (
+        <Hello key={index} name={data.name} message={data.message} />
+      ))}
+
+      <Contact email="Kittiphop@gmail.com" phon="0631871262" />
     </div>
   );
 }
 
 export default App;
-
