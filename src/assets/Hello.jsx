545f4cf6f602c020da2e5cd208e2c051ae15ef9e
kittiphopp <parkpor26@gmail.com>
2024-02-28 10:07:53 +0700

    .

--- src/assets/Hello.jsx
+++ src/assets/Hello.jsx
@@ -1,9 +1,20 @@
-function Hello() {
+
+const Hello = ({ message, name }) => {
+    console.log({ message, name });
     return (
         <div>
-           <h1>Hello World</h1>
+            <h1>
+                {message} {name}
+            </h1>
         </div>
     );
-}
+};
+
+import PropTypes from 'prop-types';
+
+Hello.PropTypes = {
+    message: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+};
 
 export default Hello;
